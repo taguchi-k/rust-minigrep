7475905548c0be000981cbd3bdc9a3b9f02b10f1
kentaro <taguchi-k@users.noreply.github.com>
2024-07-08 16:01:42 +0900

    add error handle

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,44 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    // {}を探しています
-    println!("Searching for {}", query);
-    // {}というファイルの中
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    // ファイルが見つかりませんでした
-    let mut f = File::open(filename).expect("file not found");
+    let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)
-        // ファイルの読込中に問題がありました
         .expect("something went wrong reading the file");
 
-    // テキストは \n{} です
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = &args[1].clone();
+        let filename = &args[2].clone();
+
+        Ok(Config {
+            query: query.to_string(),
+            filename: filename.to_string(),
+        })
+    }
 }
