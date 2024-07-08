3cb7deafa11d2b2d61bed8e78748b1c065b4900c
kentaro <taguchi-k@users.noreply.github.com>
2024-07-08 15:01:54 +0900

    implemnt query and filename log
    ref: https://doc.rust-jp.rs/book-ja/ch12-01-accepting-command-line-arguments.html

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    // {}を探しています
+    println!("Searching for {}", query);
+    // {}というファイルの中
+    println!("In file {}", filename);
 }
